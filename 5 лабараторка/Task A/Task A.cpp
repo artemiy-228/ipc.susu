369b3afe529d2f9e4d54663f1672ce71225aba20
artemiy-228 <79500772+artemiy-228@users.noreply.github.com>
2022-11-06 23:30:02 +0500

    Update Task A.cpp

--- 5 лабараторка/Task A/Task A.cpp
+++ 5 лабараторка/Task A/Task A.cpp
@@ -1,16 +1,16 @@
-#include iostream
+#include <iostream>
 
 using namespace std;
 
 int main(){
    int n;
-   cin  n;
+   cin >> n;
    double k, m, sum;
-   for(k = 1; k = n; k++){
-      for(m = 1; m = n;m++){
-      sum += n  (2  k + m);
+   for(k = 1; k <= n; k++){
+      for(m = 1; m <= n;m++){
+      sum += n / (2 * k + m);
       }
    }
    cout.precision(6);
-   cout  fixed  sum;
+   cout << fixed << sum;
 }
